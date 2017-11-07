6a641218360ab187ee57709b8d5c368abe1a01df
e6420 light <e6420 light@e6420light-PC>
2017-11-07 07:21:11 +0200

    first commitment. not final; system.out.println shows not correct result 115Alex"/"

---
+++ src/org/itstep/AppRunner.java
@@ -0,0 +1,12 @@
+package org.itstep;
+
+public class AppRunner {
+
+	public static void main(String[] args) {
+		
+		char b = '\"';
+		char c = '/';
+				
+			System.out.println (+ b + c + b + " Alex " + b + c + b);
+	}
+}
