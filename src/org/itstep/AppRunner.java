e9970317b03764ef5f42285d813f736ccedb3a51
e6420 light <e6420 light@e6420light-PC>
2017-11-11 14:33:17 +0200

    added different method with conversion

--- src/org/itstep/AppRunner.java
+++ src/org/itstep/AppRunner.java
@@ -7,9 +7,12 @@ public class AppRunner {
 		char b = '"';
 		char c = '/';
 		char f = 92;
+		String one = String.valueOf(f);
+		String two = String.valueOf(b);
 				
 			System.out.println (""+ b + c + b + " Alex " + b + c + b);
 			System.out.println (""+b + f + b + " Alex " + b + f + b);
 			System.out.println (+b + f + b + " Alex " + b + f + b);
+			System.out.println (two + one + two + " Alex " + two + one + two);
 	}
 }
